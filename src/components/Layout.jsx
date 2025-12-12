1d9b4dd20fa95417d965048268d2327035666b16
John Andrew Divina <98078051+LazyAndrew1333@users.noreply.github.com>
2025-12-13 00:28:23 +0800

    Fix import path for layout CSS file

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -4,7 +4,7 @@ import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
 import Footer from "./Footer";
 
-import "../style/layout.css";
+import "../style/Layout.css";
 
 const Layout = () => {
   return (
